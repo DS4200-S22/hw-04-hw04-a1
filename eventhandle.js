e771e150966274f6f1ff855fadb3064358027b2a
hdench <HenryFDench@gmail.com>
2022-02-16 16:31:43 -0500

    MouseOver and MouseOut

--- eventhandle.js
+++ eventhandle.js
@@ -1,18 +1,38 @@
+
+// Circle 1 Counter
 let numClicks = 0;
 
 function circ1Click(){
 	numClicks += 1;
-	document.getElementById("clicked").innerHTML = numClicks;
+	document.getElementById("circ1Clicked").innerHTML = numClicks;
 };
 
+
+// moused over two functions
+function circ2Mouse(){
+
+    document.getElementById("circ2Over").style.border = "2px solid black";
+
+}
+
+function circ2MouseOut(){
+
+    document.getElementById("circ2Over").style.border = "0";
+
+}
+
+// Circle Disapearing
 let circOpacity = true;
+
 function circ3Click(){
 	
+    // if showing, make opacity = 0 , if not showing, make opacity = 1
 	if(circOpacity) {
-		document.getElementById("circ3").style.opacity = "1.0";
+		document.getElementById("circ3Clicked").style.opacity = "0.0";
 	}
 	else {
-		document.getElementById("circ3").style.opacity = "0.0";
+		document.getElementById("circ3Clicked").style.opacity = "1.0";
 	}
     circOpacity = !circOpacity;
+    
 };
