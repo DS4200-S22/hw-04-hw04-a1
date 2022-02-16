912b4c9e2f9ded791f5ad1073767bc9eea637754
hdench <HenryFDench@gmail.com>
2022-02-16 15:59:48 -0500

    errors

--- eventhandle.js
+++ eventhandle.js
@@ -2,17 +2,17 @@ let numClicks = 0;
 
 function circ1Click(){
 	numClicks += 1;
-	document.getElementById("clicksed").innerHTML = numClicks;
+	document.getElementById("clicked").innerHTML = numClicks;
 };
 
 let circOpacity = true;
 function circ3Click(){
 	
 	if(circOpacity) {
-		document.getElementById("but3").style.opacity = "1.0";
+		document.getElementById("circ3").style.opacity = "1.0";
 	}
 	else {
-		document.getElementById("but3").style.opacity = "0.0";
+		document.getElementById("circ3").style.opacity = "0.0";
 	}
     circOpacity = !circOpacity;
 };
